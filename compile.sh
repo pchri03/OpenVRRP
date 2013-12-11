087f2f371c634c7d372224f5e2a1c263bd7a5454
Peter Christensen <pch@ordbogen.com>
2013-12-11 19:58:51 +0100

    Fixed typo

--- compile.sh
+++ compile.sh
@@ -17,7 +17,7 @@ fi
 if [ $LIBNL3 -eq 1 ]; then
 	$CXX -std=c++0x $CXXFLAGS `pkg-config --cflags libnl-route-3.0` -DLIBNL3 -c -o $*
 elif [ $LIBNL2 -eq 1 ]; then
-	$CXX -std=c++0x $CXXFLAGS `pkg-config --cflags libnl-2.0` -DLIBNL2-c -o $*
+	$CXX -std=c++0x $CXXFLAGS `pkg-config --cflags libnl-2.0` -DLIBNL2 -c -o $*
 else
 	echo "OpenVRRP requires libnl version 3 or 2" >&2
 	exit 1
